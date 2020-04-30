ecb1e3e9560cb85530119a51f4ad79a058470add
MadMcCrow <https://MadMcCrow@github.com>
2020-04-30 20:34:24 +0200

    initial project setup

---
+++ src/core/core.hpp
@@ -0,0 +1,5 @@
+// Copyright (c) 2020 Noé Perard-Gayot. All rights reserved.
+// This work is licensed under the terms of the MIT license. 
+// For a copy, see <https://opensource.org/licenses/MIT>.
+
+/// This file contains the most basic declaration of this 
